d1ac6717fffd11e175dc5a431243d02142247ca7
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-08-27 18:34:55 -0700

    Add test and function for calculator

--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,6 @@
 import { capitalize } from "./capitalize";
 import { reverseString } from "./reverseString";
+import { calculator } from "./calculator";
 
 test('capitalizes first letter of word', () => {
 	expect(capitalize("dog")).toBe("Dog");
@@ -8,3 +9,19 @@ test('capitalizes first letter of word', () => {
 test('reverses a string', () => {
 	expect(reverseString("Odin, I am almost finished with you.")).toBe(".uoy htiw dehsinif tsomla ma I ,nidO")
 });
+
+test('add two numbers', () => {
+	expect(calculator.add(3, 4)).toBe(7)
+})
+
+test('subtract two numbers', () => {
+	expect(calculator.sub(7, 2)).toBe(5)
+})
+
+test('multiply two numbers', () => {
+	expect(calculator.mul(7, 6)).toBe(42)
+})
+
+test('divide two numbers', () => {
+	expect(calculator.div(18, 2)).toBe(9)
+})
